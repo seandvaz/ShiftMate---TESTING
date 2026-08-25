ed286a4d5ae27f9f46c07bb3d73ba39e6db57ffa
seandvaz <lukedvaz@hotmail.com>
2026-08-26 02:48:47 +0800

    2.3.13

---
+++ service-worker.js
@@ -0,0 +1,10 @@
+const CACHE='shiftmate-2.3.13-test';
+const SHELL=['./','./index.html','./styles.css','./app.js','./shiftData.js','./payCalculator.js','./agreementData.js','./storage.js','./icon-192.png','./icon-512.png','./apple-touch-icon.png','./manifest.json','./leave-form-template.png'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+  event.respondWith(fetch(event.request).then(response=>{
+    const copy=response.clone();caches.open(CACHE).then(c=>c.put(event.request,copy));return response;
+  }).catch(()=>caches.match(event.request).then(r=>r||caches.match('./index.html'))));
+});
